e65c727f61d278b52d07ed7915593c9346012d54
Lee-HyeonCheol <165495317+Lee-HyeonCheol@users.noreply.github.com>
2024-07-21 16:28:16 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.17 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
@@ -11,7 +11,7 @@ vector<int> solution(vector<int> numbers) {
     sort(numbers.begin(), numbers.end());
 
     for (int i = 0; i < numbers.size(); i++) {
-        for (int j = 0; j < numbers.size(); j++) {
+        for (int j = i + 1; j < numbers.size(); j++) {
             if (i == j)
                 continue;
             answer.push_back(numbers[i] + numbers[j]); 
