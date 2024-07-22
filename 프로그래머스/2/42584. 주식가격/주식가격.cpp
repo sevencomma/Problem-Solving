0f4204ddda61f193378d6e206852cb347acdc64b
Lee-HyeonCheol <165495317+Lee-HyeonCheol@users.noreply.github.com>
2024-07-22 22:03:26 +0900

    [level 2] Title: 주식가격, Time: 26.33 ms, Memory: 26.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class price_dot {
+public:
+    int index;
+    int price;
+};
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer(prices.size(), -1);
+
+    stack<price_dot> price_dot;  
+
+    for (int i = 0; i < prices.size(); i++) {
+        if (price_dot.empty() || price_dot.top().price <= prices[i]) {
+            price_dot.push({ i, prices[i] });
+        }
+        else {
+            while (!price_dot.empty() && price_dot.top().price > prices[i]) {
+                answer[price_dot.top().index] = i - price_dot.top().index;
+                price_dot.pop();
+            }
+            price_dot.push({ i, prices[i] });
+        }
+    }
+   
+    for (int i = 0; i < price_dot.size(); i++) {
+        answer[price_dot.top().index] = prices.size() - 1 - price_dot.top().index;
+        price_dot.pop();
+    }
+
+    for (int i = 0; i < answer.size(); i++) {
+        if (answer[i] == -1)
+            answer[i] = prices.size() - 1 - i;
+    }
+
+
+    return answer;
+}
