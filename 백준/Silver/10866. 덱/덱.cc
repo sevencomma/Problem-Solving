63006856974d593cf633e90be2ea3cb974549ae5
Lee-HyeonCheol <165495317+Lee-HyeonCheol@users.noreply.github.com>
2024-07-26 12:26:12 +0900

    [Silver IV] Title: 덱, Time: 348 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	deque<int> deque;
+	int N;
+	cin >> N;
+	cin.ignore();
+
+	string command;
+
+	for (int i = 0; i < N; i++) {
+		getline(cin, command);
+		int val; 
+
+		if (command == "pop_front") {
+			if (deque.empty()) {
+				val = -1;
+				cout << val << "\n";
+				continue;
+			}
+			cout << deque.front() << "\n";
+			deque.pop_front();
+		}
+		else if (command == "pop_back") {
+			if (deque.empty()) {
+				val = -1;
+				cout << val << "\n";
+				continue;
+			}
+			cout << deque.back() << "\n";
+			deque.pop_back();
+		}
+		else if (command == "size") {
+			cout << deque.size() << "\n";
+		}
+		else if (command == "empty") {
+			val = deque.empty() ? 1 : 0;
+			cout << val << "\n";
+		}
+		else if (command == "front") {
+			if (deque.empty()) { 
+				val = -1; 
+				cout << val << "\n";  
+				continue; 
+			}
+			cout << deque.front() << "\n"; 
+		}
+		else if (command == "back") {
+			if (deque.empty()) {  
+				val = -1;  
+				cout << val << "\n"; 
+				continue; 
+			}
+			cout << deque.back() << "\n"; 
+		}
+		else if (command[5] == 'f') {	// push_front
+			int x = stoi(command.substr(11));
+
+			deque.push_front(x); 
+		}
+		else {	// push_back
+			int x = stoi(command.substr(10));
+
+			deque.push_back(x);
+		}
+	}
+
+}
