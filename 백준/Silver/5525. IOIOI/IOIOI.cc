4ff7d4016a267aabf9e2550fc06d5a722b6ec6ba
Lee-HyeonCheol <165495317+Lee-HyeonCheol@users.noreply.github.com>
2024-08-30 19:28:37 +0900

    [Silver I] Title: IOIOI, Time: 36 ms, Memory: 4916 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -1,45 +1,31 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <map>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(void)
 {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-
 	int N, M;
-
 	cin >> N >> M;
-	
-	string pstr = "";
-	for (int i = 0; i < N; i++) {
-		pstr += "IO";
-	}
-	pstr += "I";
+	cin.ignore();
 
-	vector<char> sentence(M);
+	string sentence;
+	getline(cin, sentence);
 
+
+	int answer = 0;
 	for (int i = 0; i < M; i++) {
-		cin >> sentence[i];
-	}
+		if (sentence[i] == 'O')
+			continue;
 
-	int length = pstr.length();
-	int count = 0;
-	for (int i = 0; i <= M - length; i++) {
-		int k = 0; 
-		int j = i;
-		while (pstr[k] == sentence[j]) {
-			k++;
-			j++;
-			
-			if (k == length) {
-				count++;
-				break;
-			}
+		int count = 0;
+		while (i + 2 < M && sentence[i] == 'I' && sentence[i+1] == 'O' && sentence[i+2] == 'I') {
+			count++;
+			i += 2;
+		}
+		if (count >= N) {
+			answer += count - N + 1;
+			if (sentence[i] != 'I')
+				answer--;
 		}
 	}
-	cout << count;
+	cout << answer;
 }
