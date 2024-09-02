c53ffecd5bfc07cc37483a75466d72e72a3e2d16
Lee-HyeonCheol <165495317+Lee-HyeonCheol@users.noreply.github.com>
2024-09-02 13:11:14 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 104 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct point {
+	int x, y;
+};
+
+bool compare(const point& a, const point& b) {
+	if (a.y == b.y)
+		return a.x < b.x;
+	return a.y < b.y;
+}
+
+int main(void)
+{
+	int n;
+	cin >> n;
+
+	vector<point> points;
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+
+		points.push_back({ x,y });
+	}
+
+	sort(points.begin(), points.end(), compare);
+
+	for (auto& it : points)
+		cout << it.x << " " << it.y << "\n";
+}
