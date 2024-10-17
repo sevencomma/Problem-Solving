e3dc0a301bee9c7b99b8330e13b4ef5e30dc48fa
Lee-HyeonCheol <165495317+Lee-HyeonCheol@users.noreply.github.com>
2024-10-17 22:50:41 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int a, b;
+	while (1) {
+		cin >> a >> b;
+		if (a == 0 && b == 0)
+			break;
+		cout << a + b << '\n';
+	}
+}
