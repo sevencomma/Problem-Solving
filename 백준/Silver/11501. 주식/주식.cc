5b308f8f2bce961538e9c2a7d9a373c1470d5fe5
Lee HyeonCheol <22311963@yu.ac.kr>
2025-08-22 12:11:12 +0900

    [Silver II] Title: 주식, Time: 2636 ms, Memory: 22416 KB -BaekjoonHub

---
+++ 백준/Silver/11501. 주식/주식.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int T, N;
+
+struct point {
+	int cost, idx;
+};
+
+struct cmp {
+	bool operator() (point& a, point& b) const {
+		if (a.cost != b.cost)
+			return a.cost < b.cost;
+		return a.idx > b.idx;
+	}
+};
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin >> T;
+	while (T--) {
+		cin >> N;
+		vector<int> arr(N);
+		for (int i = 0; i < N; i++)
+			cin >> arr[i];
+
+		priority_queue<point, vector<point>, cmp> pq;
+		for (int i = 0; i < N; i++)
+			pq.push({ arr[i], i });
+
+		priority_queue<int> has;
+		long long res = 0;
+		for (int i = 0; i < N; i++) {
+			if (!pq.empty() && pq.top().idx > i) { // 사
+				has.push(arr[i]);
+			}
+			else {	// 팔아 or 냅둬
+				while (!has.empty()) {
+					res += arr[i] - has.top();
+					has.pop();
+				}
+				while (!pq.empty() && pq.top().idx <= i)
+					pq.pop();
+			}
+		}
+		cout << res << '\n';
+	}
+}
