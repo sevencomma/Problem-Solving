d53fb8faee84bfa5c5203c4a43514a4b1ee8311c
현철 <22311963@yu.ac.kr>
2025-11-27 10:11:46 +0900

    [Gold II] Title: 보석 도둑, Time: 160 ms, Memory: 16488 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,52 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct point {
-	int m, v;
+typedef long long ll;
 
-	bool operator < (point a) const {
-		return v < a.v;
+struct dat {
+	int m, v;
+};
+
+struct cmpV {
+	bool operator() (dat& a, dat& b) {
+		return a.v < b.v;
 	}
 };
 
-bool compare(point a, point b) {
+bool cmpM(dat& a, dat& b) {
 	return a.m < b.m;
 }
+
+int N, K, m, v, x;
+vector<dat> boseok;
+vector<int> backpack;
 
 int main(void) {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	int N, K;
 	cin >> N >> K;
-	vector<point> boseok;
-	vector<int> backpack(K);
-
 	for (int i = 0; i < N; i++) {
-		int m, v;
 		cin >> m >> v;
-		boseok.push_back({ m, v });
+		boseok.push_back({m, v});
 	}
-	for (int i = 0; i < K; i++)
-		cin >> backpack[i];
+	for (int i = 0; i < K; i++) {
+		cin >> x;
+		backpack.push_back(x);
+	}
 
-	sort(boseok.begin(), boseok.end(), compare);
+	sort(boseok.begin(), boseok.end(), cmpM);
 	sort(backpack.begin(), backpack.end());
 
-	long long sum = 0;
-	priority_queue<point> q;
-	int index = 0;
+	priority_queue<dat, vector<dat>, cmpV> pq;
+	int idx = 0;
+	ll res = 0;
+
 	for (int i = 0; i < K; i++) {
 		int size = backpack[i];
 
-		while (index < N && size >= boseok[index].m) {
-			q.push(boseok[index]);
-			index++;
+		while (idx < N && boseok[idx].m <= size) {
+			pq.push(boseok[idx]);
+			idx++;
 		}
-		if (q.empty())
-			continue;
-		point cur = q.top();
-		q.pop();
-		sum += cur.v;
+
+		if (!pq.empty()) {
+			res += pq.top().v;
+			pq.pop();
+		}
 	}
-	cout << sum;
+	cout << res;
 }
